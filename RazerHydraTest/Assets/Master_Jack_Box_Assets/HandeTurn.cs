d766849bf898b41113a7cc59d34a4b424cb7545c
vector8 <vector8@localhost>
2015-12-11 20:22:00 -0500

    Added Jack-in-the-box, modified project settings to support oculus.

---
+++ RazerHydraTest/Assets/Master_Jack_Box_Assets/HandeTurn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandeTurn : MonoBehaviour
+{
+    public float turnVelocity;
+    public float TimerSync;
+    // Use this for initialization
+    void Start()
+    {
+        TimerSync = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(TimerSync + 18f > Time.time)
+        transform.Rotate(transform.forward, turnVelocity * Time.deltaTime);
+    }
+}
