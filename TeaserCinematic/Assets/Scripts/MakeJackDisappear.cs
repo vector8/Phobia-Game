b0e2458139d4634c12b85308f0838ee0b8324c88
Chris <Chris@localhost>
2016-01-11 15:47:19 -0500

    Created a teaser cinematic for the game.

---
+++ TeaserCinematic/Assets/Scripts/MakeJackDisappear.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeJackDisappear : MonoBehaviour
+{
+    public const float DISAPPEAR_TIME = 22.5f;
+
+    private float timer = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= DISAPPEAR_TIME)
+            this.gameObject.SetActive(false);
+    }
+}
---
+++ TeaserCinematic/Assets/Scripts/TrailerCameraPan.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrailerCameraPan : MonoBehaviour
+{
+    public const float CAMERA_PAN_DURATION = 20.5f;
+
+    public Vector3 startPos, endPos;
+
+    public Transform lookAtTarget;
+
+    private float timer = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.transform.position = startPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > CAMERA_PAN_DURATION)
+            timer = CAMERA_PAN_DURATION;
+
+        Vector3 currentPos = Vector3.Lerp(startPos, endPos, timer / CAMERA_PAN_DURATION);
+        transform.position = currentPos;
+
+        transform.LookAt(lookAtTarget);
+    }
+}
