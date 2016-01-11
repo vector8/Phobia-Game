caa1db2cd92e4390735055731a07d87c037ab5ba
Chris <vector8@users.noreply.github.com>
2016-01-11 15:47:19 -0500

    Created a teaser cinematic for the game.

---
+++ TeaserCinematic/Assets/Master_Jack_Box_Assets/BoxTop_S.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxTop_S : MonoBehaviour
+{
+    public const float DURATION = 22.5f;//11.2f;
+
+    public float m_rotateVelocity;
+
+    private float timer = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (transform.rotation.z < 0.90f && timer > DURATION)
+        {
+            transform.Rotate(transform.forward, m_rotateVelocity * Time.deltaTime);
+        }
+    }
+}
---
+++ TeaserCinematic/Assets/Master_Jack_Box_Assets/HandeTurn.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandeTurn : MonoBehaviour
+{
+    public const float DURATION = 30.5f;//18f;
+
+    public float turnVelocity;
+    public float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer < DURATION)
+            transform.Rotate(transform.forward, turnVelocity * Time.deltaTime);
+    }
+}
