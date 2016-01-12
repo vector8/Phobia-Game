8da697140818bb3ef85afe790466dd318d1333e9
Chris <vector8@users.noreply.github.com>
2016-01-12 16:49:27 -0500

    Added interaction mechanic and test Door script.

---
+++ RazerHydraTest/Assets/Game Assets/Scripts/Door.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Door : Interactable
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void activate()
+    {
+        print("door has been activated");
+    }
+}
---
+++ RazerHydraTest/Assets/Game Assets/Scripts/Interactable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Interactable : MonoBehaviour
+{
+    public const float STANDARD_ACTIVATION_RANGE = 5f;
+
+    public abstract void activate();
+
+    public float getActivationRange()
+    {
+        return STANDARD_ACTIVATION_RANGE;
+    }
+}
