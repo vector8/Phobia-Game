24cd02b5a2365056b849c2fc9f8b2d27ddb674f4
Chris <Chris@localhost>
2016-01-12 16:49:27 -0500

    Added interaction mechanic and test Door script.

---
+++ RazerHydraTest/Assets/Game Assets/Scripts/InteractionManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractionManager : MonoBehaviour
+{
+    private Interactable target = null;
+
+    public GameObject interactionIcon;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2f, Screen.height / 2f));
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Interactable i = hit.transform.gameObject.GetComponent<Interactable>();
+            if (i != null && Vector3.Distance(this.transform.position, i.transform.position) < i.getActivationRange())
+            {
+                target = i;
+                interactionIcon.SetActive(true);
+            }
+            else
+            {
+                target = null;
+                interactionIcon.SetActive(false);
+            }
+        }
+
+        if (target != null && Input.GetKeyDown(KeyCode.E))
+        {
+            target.activate();
+        }
+    }
+}
