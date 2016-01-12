07f41d8343719df43421289792a515ac44508de3
Zachary Hills <25306965+Zac-hills@users.noreply.github.com>
2016-01-12 18:45:53 -0500

    Added dresser with collider and Drawer script

---
+++ RazerHydraTest/Assets/Game Assets/Scripts/Drawer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Drawer : Interactable
+{
+    bool m_open = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void activate()
+    {
+        if (m_open)
+        {
+            transform.Translate(0f,0f,(gameObject.transform.forward.z * -0.5f));
+            m_open = false;
+        }
+        else
+        {
+            transform.Translate(0f, 0f, gameObject.transform.forward.z * 0.5f);
+            m_open = true;
+        }
+    }
+}
