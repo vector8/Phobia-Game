d5f382ff7617042f89044b6f4c359e8e002a28c8
Chris <vector8@users.noreply.github.com>
2016-01-16 21:14:46 -0500

    Added batteries and reloading functionality.

---
+++ Phobia/Assets/Game Assets/Scripts/Battery.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Battery : Interactable
+{
+    private Player player;
+
+    void Start()
+    {
+        GameObject playerGO = GameObject.Find("Player");
+        player = playerGO.GetComponent<Player>();
+    }
+
+    public override void activate()
+    {
+        player.numberOfBatteries++;
+        Destroy(gameObject);
+    }
+}
