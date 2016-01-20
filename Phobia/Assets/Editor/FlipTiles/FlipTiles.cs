a327c715e35b37d5d8c7dbcfe88d5b9d017c3172
Chris <vector8@users.noreply.github.com>
2016-01-20 18:18:09 -0500

    Added new wall, door frame, door, and bookcase.

---
+++ Phobia/Assets/Editor/FlipTiles/FlipTiles.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.IO;
+
+public class FlipTiles : EditorWindow
+{
+    [MenuItem("Custom Windows/Flip Tiles")]
+    public static void showWindow()
+    {
+        EditorWindow.GetWindow(typeof(FlipTiles));
+    }
+
+    void OnGUI()
+    {
+
+        if (GUILayout.Button("Flip Selected Tiles"))
+        {
+            foreach (GameObject go in Selection.gameObjects)
+            {
+                Vector3 rot = go.transform.localEulerAngles;
+                rot.z = 0;
+                go.transform.localEulerAngles = rot;
+            }
+        }
+    }
+}
