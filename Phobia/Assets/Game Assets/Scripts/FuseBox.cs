d0b7526e8f9c24fda4de14688ad5d05cf9fb92e6
Chris <vector8@users.noreply.github.com>
2016-01-27 13:53:10 -0500

    Added fusebox, fuse, chandelier, and wood floors. Added activation logic for fusebox door. Reorganized some files and prefabs.

---
+++ Phobia/Assets/Game Assets/Scripts/FuseBox.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FuseBox : Interactable
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void activate()
+    {
+
+    }
+}
