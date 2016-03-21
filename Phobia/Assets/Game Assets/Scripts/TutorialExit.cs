09f33e3b06742a7d24f39617276fe21f5a13960c
Chris <vector8@users.noreply.github.com>
2016-03-21 13:08:39 -0400

    Modified tutorial area and added color correction test.

---
+++ Phobia/Assets/Game Assets/Scripts/TutorialExit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialExit : MonoBehaviour
+{
+    public EntryDoor door;
+    public GameObject tutorialArea;
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.tag == "Player")
+        {
+            door.activate(false);
+
+            // disable the tutorial area to prevent fuckery
+            tutorialArea.SetActive(false);
+        }
+    }
+}
