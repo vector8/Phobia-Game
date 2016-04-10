b9e8aa8b7dfc8d545c2c09ab9af287ee87a22698
Chris <vector8@users.noreply.github.com>
2016-04-10 14:18:32 -0400

    Started on menu screens.

---
+++ Phobia/Assets/Game Assets/Scripts/EntryScreen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EntryScreen : MonoBehaviour
+{
+    public GameObject queueScreen;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKey)
+        {
+            queueScreen.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Phobia/Assets/Game Assets/Scripts/QueueScreen.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class QueueScreen : MonoBehaviour
+{
+    public Button playButton, humanButton, monsterButton;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void humanButtonPressed()
+    {
+        playButton.interactable = true;
+        humanButton.interactable = false;
+        monsterButton.interactable = true;
+    }
+
+    public void monsterButtonPressed()
+    {
+        playButton.interactable = true;
+        humanButton.interactable = true;
+        monsterButton.interactable = false;
+    }
+
+    public void playButtonPressed()
+    {
+        
+    }
+}
